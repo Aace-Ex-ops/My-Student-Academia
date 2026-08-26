33caf2a27d15a2aa8492d1cfe2ca286c685ea98c
Aditya <adi99chat@gmail.com>
2026-08-26 13:55:49 +0530

    chore(auth): wipe all accounts and invalidate client browser sessions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ import { InstructorPage } from './pages/InstructorPage';
 import { AdminPage } from './pages/AdminPage';
 import { User, OnboardingData } from './types';
 
-const MSA_SESSION_VERSION = "v2026_08_26_clean";
+const MSA_SESSION_VERSION = "v2026_08_26_wipe2";
 
 export function AppContent() {
   const [users, setUsers] = useState<User[]>([]);
